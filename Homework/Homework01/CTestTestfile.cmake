014a3740dabcac287c369f938f1564323342803e
cassaram <michael@cassara.tech>
2022-01-19 22:46:04 -0500

    Homework01 - Build

---
+++ Homework/Homework01/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/ece0302-mjc/Homework/Homework01
+# Build directory: /home/user/Desktop/ece0302-mjc/Homework/Homework01
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest "bag-tests")
+set_tests_properties(unittest PROPERTIES  _BACKTRACE_TRIPLES "/home/user/Desktop/ece0302-mjc/Homework/Homework01/CMakeLists.txt;16;add_test;/home/user/Desktop/ece0302-mjc/Homework/Homework01/CMakeLists.txt;0;")
