eb6163fa7dc6c388c07f137e3e09836b937c9878
cassaram <michael@cassara.tech>
2022-02-06 18:36:04 -0500

    Homework03 - Upload first FancyPowers test

--- Homework/Homework03/egyptian_powers_starter_code/fancyPower.hpp
+++ Homework/Homework03/egyptian_powers_starter_code/fancyPower.hpp
@@ -1,9 +1,27 @@
 #ifndef FANCYPOWER_HPP
 #define FANCYPOWER_HPP
 
+/**
+ * @brief Function to calculate a power via Egyptian Powers
+ *
+ * @param n Base
+ * @param m Exponent
+ * @return int Result of power operation
+ */
 int fancyPower(int n, int m)
 {
-	// your code should go here.
+	// Check if power is 1
+	if (m == 1) {
+		return n;
+	} else {
+		if (m % 2 == 0) {
+			// Even Power
+			return fancyPower(n*n, n/2);
+		} else {
+			// Odd Power
+			return fancyPower(n*n, (n-1)/2);
+		}
+	}
 	return 0;
 }
 
--- Homework/Homework03/egyptian_powers_starter_code/fancyPower_tests.cpp
+++ Homework/Homework03/egyptian_powers_starter_code/fancyPower_tests.cpp
@@ -5,7 +5,7 @@
 
 // write your test cases here
 TEST_CASE("Test case", "fancyPower"){
-
-  REQUIRE(true);
-  
+    REQUIRE(true);
+    REQUIRE(fancyPower(2, 2) == 4);
+    REQUIRE(fancyPower(3, 2) == 9);
 }
