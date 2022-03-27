3d1c5dfeb2eee36c7aba2e09893461410ccf369c
cassaram <michael@cassara.tech>
2022-03-27 16:26:35 -0400

    Homework08 - SortedList w/ Tests

--- Homework/Homework08/priority_queue_starter_code/sorted_list.tpp
+++ Homework/Homework08/priority_queue_starter_code/sorted_list.tpp
@@ -3,73 +3,112 @@
 template <typename T, typename L>
 SortedList<T, L>::SortedList()
 {
-  // nothing to do, plist gets created on the stack
+    // nothing to do, plist gets created on the stack
 }
 
 template <typename T, typename L>
 SortedList<T, L>::SortedList(const SortedList<T, L> & x)
 {
-  plist = x.plist; // use copy assignment to implement copy construction
+    plist = x.plist; // use copy assignment to implement copy construction
 }
 
 template <typename T, typename L>
 SortedList<T, L>& SortedList<T, L>::operator=(const SortedList<T, L>& x)
 {
-  plist = x.plist;
-  return *this;
+    plist = x.plist;
+    return *this;
 }
 
 template <typename T, typename L>
 SortedList<T, L>::~SortedList()
 {
-  // plist destructor gets called automatically
+    // plist destructor gets called automatically
 }
 
 template <typename T, typename L>
 bool SortedList<T, L>::isEmpty()
 {
-  return plist.isEmpty();
+    return plist.isEmpty();
 }
 
 template <typename T, typename L>
 std::size_t SortedList<T, L>::getLength()
 {
-  return plist.getLength();
+    return plist.getLength();
 }
 
 template <typename T, typename L>
 void SortedList<T, L>::insert(const T& item)
 {
-  // TODO
+    // Find position to insert to
+    std::size_t pos = getPosition(item);
+
+    // Find abs of pos
+    if (pos < 0) {
+        pos = pos * -1;
+    }
+
+    // Perform insertion
+    plist.insert(pos, item);
 }
 
 template <typename T, typename L>
 void SortedList<T, L>::remove(const T& item)
 {
-  // TODO
+    // Find position of item
+    std::size_t pos = getPosition(item);
+
+    // Ensure item exists
+    if (pos < 0) {
+        // Item does not exist, return
+        return;
+    }
+
+    // Remove item
+    removeAt(pos);
 }
 
 template <typename T, typename L>
 void SortedList<T, L>::removeAt(std::size_t position)
 {
-  plist.remove(position);
+    plist.remove(position);
 }
 
 template <typename T, typename L>
 void SortedList<T, L>::clear()
 {
-  plist.clear();
+    plist.clear();
 }
 
 template <typename T, typename L>
 T SortedList<T, L>::getEntry(std::size_t position)
 {
-  return plist.getEntry(position);
+    return plist.getEntry(position);
 }
 
 template <typename T, typename L>
 long int SortedList<T, L>::getPosition(const T& newValue)
 {
-  // TODO
-  return 0;
+    // Determine result variable
+    std::size_t result = 0;
+
+    // Find position via linear search
+    for (std::size_t i = 0; i < getLength(); i++) {
+        // Update result
+        result = i;
+        // Determine if we are at position
+        if (newValue > getEntry) {
+            // At position, break
+            break;
+        }
+    }
+
+    // Determine if we found the item
+    if (getEntry(result) == newValue) {
+        // Item is good
+        return result;
+    } else {
+        // Item would be inserted here, negate
+        return (result * -1);
+    }
 }
--- Homework/Homework08/priority_queue_starter_code/test_sorted_list.cpp
+++ Homework/Homework08/priority_queue_starter_code/test_sorted_list.cpp
@@ -5,8 +5,123 @@
 #include "dynamic_array_list.h"
 #include "sorted_list.h"
 
-TEST_CASE("Testing Sorted List", "[sorted list]") {
-  typedef SortedList<int, DynamicArrayList<int> > slst;
-  
-  // TODO
+typedef SortedList<int, DynamicArrayList<int> > slst;
+
+TEST_CASE("Test SortedList constructor", "[sorted list]") {
+    slst l;
+    REQUIRE(l.isEmpty());
+}
+
+TEST_CASE("Test SortedList copy constructor", "[sorted list]") {
+    slst l1;
+    REQUIRE(l1.isEmpty());
+    l1.insert(0);
+    REQUIRE(!l1.isEmpty());
+    slst l2(l1);
+    REQUIRE(l2.getLength() == l1.getLength());
+    REQUIRE(l2.isEmpty());
+}
+
+TEST_CASE("Test SortedList copy assignment", "[sorted list]") {
+    slst l1;
+    REQUIRE(l1.isEmpty());
+    l1.insert(0);
+    REQUIRE(!l1.isEmpty());
+    slst l2 = l1;
+    REQUIRE(l2.getLength() == l1.getLength());
+    REQUIRE(l2.isEmpty());
+}
+
+TEST_CASE("Test SortedList isEmpty method", "[sorted list]") {
+    slst l1;
+    REQUIRE(l1.isEmpty());
+    l1.insert(12);
+    REQUIRE(!l1.isEmpty());
+    l1.clear();
+    REQUIRE(l1.isEmpty());
+}
+
+TEST_CASE("Test SortedList getLength method", "[sorted list]") {
+    slst l1;
+    for (std::size_t i = 0; i < 13; i++) {
+        REQUIRE(l1.getLength() == i);
+        l1.insert(i);
+    }
+    REQUIRE(l1.getLength() == 13);
+    l1.clear();
+    REQUIRE(l1.getLength() == 0);
+}
+
+TEST_CASE("Test SortedList insert method", "[sorted list]") {
+    slst l1;
+    for (std::size_t i = 13; i > 0; i--) {
+        l1.insert(i);
+    }
+    REQUIRE(l1.getLength() == 13);
+    for (std::size_t i = 0; i < 12; i++) {
+        REQUIRE(l1.getEntry(i) <= l1.getEntry(i + 1));
+    }
+}
+
+TEST_CASE("Test SortedList remove method", "[sorted list]") {
+    slst l1;
+    for (std::size_t i = 13; i > 0; i--) {
+        l1.insert(i);
+    }
+    REQUIRE(l1.getLength() == 13);
+    l1.remove(3);
+    REQUIRE(l1.getLength() == 12);
+    for (std::size_t i = 0; i < 11; i++) {
+        REQUIRE(l1.getEntry(i) <= l1.getEntry(i + 1));
+        REQUIRE(l1.getEntry(i) != 3);
+    }
+}
+
+TEST_CASE("Test SortedList removeAt method", "[sorted list]") {
+    slst l1;
+    for (std::size_t i = 13; i > 0; i--) {
+        l1.insert(i);
+    }
+    REQUIRE(l1.getLength() == 13);
+    l1.removeAt(0);
+    REQUIRE(l1.getLength() == 12);
+    for (std::size_t i = 0; i < 11; i++) {
+        REQUIRE(l1.getEntry(i) <= l1.getEntry(i + 1));
+        REQUIRE(l1.getEntry(i) != 1);
+    }
+}
+
+TEST_CASE("Test SortedList clear method", "[sorted list]") {
+    slst l1;
+    REQUIRE(l1.getLength() == 0);
+    REQUIRE(l1.isEmpty());
+    l1.insert(0);
+    l1.insert(13);
+    REQUIRE(l1.getLength() != 0);
+    REQUIRE(!l1.isEmpty());
+    l1.clear();
+    REQUIRE(l1.getLength() == 0);
+    REQUIRE(l1.isEmpty());
+}
+
+TEST_CASE("Test SortedList getEntry method", "[sorted list]") {
+    slst l1;
+    for (std::size_t i = 13; i > 0; i--) {
+        l1.insert(i);
+    }
+    for (std::size_t i = 0; i < 13; i++) {
+        REQUIRE(l1.getEntry(i) == i+1);
+    }
+}
+
+TEST_CASE("Test SortedList getPosition method", "[sorted list]") {
+    slst l1;
+    for (std::size_t i = 13; i > 0; i--) {
+        l1.insert(i);
+    }
+    for (std::size_t i = 0; i < 13; i++) {
+        REQUIRE(l1.getPosition(i+1) == i);
+    }
+
+    REQUIRE(l1.getPosition(15) == -1 * l1.getLength());
 }
