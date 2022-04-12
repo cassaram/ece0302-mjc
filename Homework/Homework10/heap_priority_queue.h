323407e476e0a21fb0868465ea3da528eadcf7b3
cassaram <michael@cassara.tech>
2022-04-11 22:29:25 -0400

    Homework 10 - First attempt

--- Homework/Homework10/heap_priority_queue.h
+++ Homework/Homework10/heap_priority_queue.h
@@ -9,48 +9,63 @@ template <typename T>
 class HeapPriorityQueue: public AbstractPriorityQueue<T>
 {
 public:
-    
+
     // return true if the priority queue is empty
     bool isEmpty();
-    
+
     // insert item into the queue
     void add(const T& item);
-    
+
     // remove highest priority item from the queue
     void remove();
-    
+
     // get the item with the highest priority from the queue
     T peek();
-    
+
 private:
-    
+
     DynamicArrayList<T> lst;
 };
 
 template <typename T>
 bool HeapPriorityQueue<T>::isEmpty()
 {
-    //todo
-    return false;
+    // Return list call
+    return lst.isEmpty();
 }
 
 template <typename T>
 void HeapPriorityQueue<T>::add(const T& item)
 {
-    //todo
+    // Get variables to iterate through heap bottom-up
+    std::size_t element = lst.getLength() + 1;
+    std::size_t parent = (element - 1) / 2;
+
+    // Iterate through heap array
+    while ((parent >= 1) && (lst.getEntry(parent) < item)) {
+        // Replace current position with parent
+        lst.setEntry(element) = lst.getEntry(parent);
+
+        // Move to next parent
+        element = parent;
+        parent = (parent - 1) / 2;
+    }
+    // Insert element
+    lst.setEntry(element) = item;
 }
 
 template <typename T>
 void HeapPriorityQueue<T>::remove()
 {
-    //todo
+    // Remove item
+    lst.remove(0);
 }
 
 template <typename T>
 T HeapPriorityQueue<T>::peek()
 {
-    //todo
-    return T();
+    // Return root at first index
+    return lst.getEntry(0);
 }
 
 
--- Homework/Homework10/test_priority_queue.cpp
+++ Homework/Homework10/test_priority_queue.cpp
@@ -1,38 +1,56 @@
 #define CATCH_CONFIG_MAIN
-#define CATCH_CONFIG_COLOUR_NONE
+//#define CATCH_CONFIG_COLOUR_NONE
 #include "catch.hpp"
 
 #include "heap_priority_queue.h"
 
 TEST_CASE("Test Construction", "[construction]") {
-  HeapPriorityQueue<int>  pq;
-  REQUIRE(pq.isEmpty());
+    HeapPriorityQueue<int>  pq;
+    REQUIRE(pq.isEmpty());
 }
 
 TEST_CASE("Test add", "[add]") {
-  HeapPriorityQueue<int>  pq;
-  pq.add(25);
+    HeapPriorityQueue<int>  pq;
+    pq.add(25);
 
-  REQUIRE(!pq.isEmpty());
-  REQUIRE(pq.peek() == 25);
+    REQUIRE(pq.peek() == 25);
+    REQUIRE(!pq.isEmpty());
 
-  pq.add(1);
-  REQUIRE(pq.peek() == 25);
+    pq.add(1);
+    REQUIRE(pq.peek() == 25);
+    REQUIRE(!pq.isEmpty());
 
-  pq.add(78);
-  REQUIRE(pq.peek() == 78);
+    pq.add(78);
+    REQUIRE(pq.peek() == 78);
+    REQUIRE(!pq.isEmpty());
+
+    pq.add(4);
+    REQUIRE(pq.peek() == 78);
+    REQUIRE(!pq.isEmpty());
+
+    pq.add(-1);
+    REQUIRE(pq.peek() == 78);
+    REQUIRE(!pq.isEmpty());
+
+    pq.add(100);
+    REQUIRE(pq.peek() == 100);
+    REQUIRE(!pq.isEmpty());
 }
 
 TEST_CASE("Test remove", "[remove]") {
-  HeapPriorityQueue<int>  pq;
-  pq.add(25);
-  pq.add(1);
-  pq.add(78);
+    HeapPriorityQueue<int>  pq;
+    pq.add(25);
+    pq.add(1);
+    pq.add(78);
 
-  pq.remove();
-  REQUIRE(pq.peek() == 25);
-  pq.remove();
-  REQUIRE(pq.peek() == 1);
-  pq.remove();
-  REQUIRE(pq.isEmpty());
+    REQUIRE(pq.peek() == 78);
+    REQUIRE(!pq.isEmpty());
+    pq.remove();
+    REQUIRE(pq.peek() == 25);
+    REQUIRE(!pq.isEmpty());
+    pq.remove();
+    REQUIRE(pq.peek() == 1);
+    REQUIRE(!pq.isEmpty());
+    pq.remove();
+    REQUIRE(pq.isEmpty());
 }
