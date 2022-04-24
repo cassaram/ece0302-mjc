7046df6cfae972d043341f2301dccbae074e8751
cassaram <michael@cassara.tech>
2022-04-24 17:59:10 -0400

    Project 5 - Build

---
+++ Projects/Project05/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Desktop/ece0302-mjc/Projects/Project05
+# Build directory: /home/user/Desktop/ece0302-mjc/Projects/Project05
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(student_tests "test_frontier_queue")
+set_tests_properties(student_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/user/Desktop/ece0302-mjc/Projects/Project05/CMakeLists.txt;21;add_test;/home/user/Desktop/ece0302-mjc/Projects/Project05/CMakeLists.txt;0;")
+add_test(solver_tests "test_solver")
+set_tests_properties(solver_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/user/Desktop/ece0302-mjc/Projects/Project05/CMakeLists.txt;22;add_test;/home/user/Desktop/ece0302-mjc/Projects/Project05/CMakeLists.txt;0;")
